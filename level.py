2073c50230f3a991108c020ab13ab6d5bf09db50
Tomi Javanainen <tomi.javanainen@gmail.com>
2017-05-07 09:50:45 +0300

    -Kommentoitu level.xml:ää -Päivitetty xml-tiedoston lukua vähän järkevämmäksi -Keskeneräinen ship.xml

--- level.py
+++ level.py
@@ -30,29 +30,29 @@ class Level(pygame.sprite.Sprite):
     def load_from_xml(self, level_name):
         level_found = 0
         root = text.read_xml('xml/level.xml')
-        for current_level in root.findall('level'):
-            if current_level.attrib['name'] == level_name:
-                print "Level", level_name, "info found"
-                level_found = 1
-                self.name = level_name
-                self.image = pygame.image.load(current_level.find('image').text).convert_alpha()
-                try:
-                    self.background_image = pygame.image.load(current_level.find('background').text).convert_alpha()
-                except AttributeError:
-                    self.background_image = None
-                self.gravity = float(current_level.find('gravity').text)
+        current_level = root.find(".//level[@name='"+level_name+"']")
+        if current_level is not None:
+            level_found = 1
+            print 'Level "' + level_name + '" info found'
+            self.name = level_name
+            self.image = pygame.image.load(current_level.find('image').text).convert_alpha()
+            try:
+                self.background_image = pygame.image.load(current_level.find('background').text).convert_alpha()
+            except AttributeError:
+                self.background_image = None
+            self.gravity = float(current_level.find('gravity').text)
 
-                self.size_x = self.image.get_width()
-                self.size_y = self.image.get_height()
-                self.rect = self.image.get_rect()
-                self.center_point = self.size_x // 2, self.size_y // 2
+            self.size_x = self.image.get_width()
+            self.size_y = self.image.get_height()
+            self.rect = self.image.get_rect()
+            self.center_point = self.size_x // 2, self.size_y // 2
 
-                self.player_spawns_team_1 = []
-                self.player_spawns_team_2 = []
+            self.player_spawns_team_1 = []
+            self.player_spawns_team_2 = []
 
-                for current_spawn in current_level.find('player_spawns').find('team1'):
-                    self.player_spawns_team_1.append((int(current_spawn.attrib['x']), int(current_spawn.attrib['y'])))
-                for current_spawn in current_level.find('player_spawns').find('team2'):
-                    self.player_spawns_team_2.append((int(current_spawn.attrib['x']), int(current_spawn.attrib['y'])))
-                break
+            for current_spawn in current_level.find('player_spawns').find('team1'):
+                self.player_spawns_team_1.append((int(current_spawn.attrib['x']), int(current_spawn.attrib['y'])))
+            for current_spawn in current_level.find('player_spawns').find('team2'):
+                self.player_spawns_team_2.append((int(current_spawn.attrib['x']), int(current_spawn.attrib['y'])))
+
         return level_found
