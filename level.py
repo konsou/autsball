bad2506eec0889f60960763d84b3e2b8ef006794
Tomi Javanainen <tomi.javanainen@gmail.com>
2017-05-08 20:29:50 +0300

    Off-level imagen mergausta tähän Background ei vielä toimi, fiksaa

--- level.py
+++ level.py
@@ -30,15 +30,45 @@ class Level(pygame.sprite.Sprite):
             self.background_image = pygame.image.load(current_level.find('background').text).convert()
         except AttributeError:
             self.background_image = None
-
-        # Gravity
-        self.gravity = float(current_level.find('gravity').text)
+        try:
+            self.off_level_image = pygame.image.load(current_level.find('off-level').text).convert()
+        except AttributeError:
+            self.off_level_image = None
 
         # Levelin koko, rect, center_point
         self.size_x = self.image.get_width()
         self.size_y = self.image.get_height()
         self.rect = self.image.get_rect()
         self.center_point = self.size_x // 2, self.size_y // 2
+
+        if self.background_image is not None:
+            self.background_image.set_colorkey(BLACK)
+            #generoidaan taustakuva
+            #background_image = pygame.image.load(background_image_file).convert_alpha()
+            self.background_image = pygame.Surface((self.size_x, self.size_y))
+            surface_rect = self.background_image.get_rect()
+            image_rect = self.background_image.get_rect()
+            for x in range(0, surface_rect.width, image_rect.width):
+                for y in range(0, surface_rect.height, image_rect.height):
+                    self.background_image.blit(self.background_image, (x, y))
+
+        # Generoidaan ulkopuolinen aines
+        self.off_level_surface = pygame.Surface((self.size_x+WINDOW_SIZE[0], self.size_y+WINDOW_SIZE[1]),
+                                                pygame.HWSURFACE)
+        surface_rect = self.off_level_surface.get_rect()
+        image_rect = self.off_level_image.get_rect()
+        for x in range(0, surface_rect.width, image_rect.width):
+            for y in range(0, surface_rect.height, image_rect.height):
+                self.off_level_surface.blit(self.off_level_image, (x, y))
+        if self.background_image:
+            self.off_level_surface.blit(self.background_image,
+                                        (WINDOW_SIZE[0]//2, WINDOW_SIZE[1]//2, self.size_x, self.size_y))
+        else:
+            self.off_level_surface.fill(BLACK, (WINDOW_SIZE[0]//2, WINDOW_SIZE[1]//2, self.size_x, self.size_y))
+        self.off_level_surface.set_colorkey(BLACK)
+
+        # Gravity
+        self.gravity = float(current_level.find('gravity').text)
 
         # Spawn pointit
         self.player_spawns = {'red': [], 'green': []}
