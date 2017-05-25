d38bc81400db6db459638e4ad912602fbf6c7ca6
Teemu Siltanen <teemu.siltanen@gmail.com>
2017-05-25 12:40:03 +0300

    Server osaa aloittaa pelin todella korkealla FPS:llä

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 
-import network
+from network import *
 import json
 from constants import *
 
@@ -8,7 +8,7 @@ from constants import *
 class Server(object):
 
     def __init__(self):
-        self._network = network.Network()
+        self._network = Network()
         # Lista clienteista, joiden viesteistä välitetään
         self._client_list = set()
         self._client_ip_id_combination = {}
@@ -28,6 +28,9 @@ class Server(object):
         self._update_interval = 20
         self._update_counter = 0
 
+        # Game instance
+        self._game_instance = None
+
     def update(self, clock):
         # Lobby-vaihe
         if self._waiting_for_client_to_join:
@@ -43,9 +46,9 @@ class Server(object):
                         client_name = client_response[0].split(":")[1]
                         client_id = {"client_id": self._client_id_counter}
                         self._client_list.add(client_response[1])
-                        self._client_ip_id_combination[client_response[1]] = client_id
+                        self._client_ip_id_combination[client_response[1][0]] = self._client_id_counter
                         self._client_id_counter += 1
-                        print "Added client with ip ", client_response[1]
+                        print "Added client with ip ", client_response[1][0]
                         print "Client name is %s" % client_name
                         # Lähetetään pelaajalle sen oma id
                         packet_to_client = json.dumps(client_id)
@@ -56,34 +59,51 @@ class Server(object):
                 pass
         # Pelin sisällä
         elif self._in_game:
-            client_inputs = set()
+            client_inputs = {}
             while self._update_counter < self._update_interval:
                 # Kuunnellaan clientteja fixattu aika ja lasketaan sitten mitä tapahtuu kaikkien syötteille
                 data_dict = self._network.server_listen()
                 if data_dict is not None:
-                    if data_dict[1] in self.client_list:
-                        data_object = data_dict[0]
+                    if data_dict[1] in self._client_list:
+                        data_object = unpack_int(data_dict[0])
                         # TODO: lisää clientin input vaikuttamaan laskuihin
                         # client_inputs setissä keynä on clientin assignattu id ja valuena clientin lähettämät input tiedot
                         # tarkistetaan onko client lähettänyt jo inputit tässä syklissä
-                        if self._client_ip_id_combination[data_dict[1]] not in client_inputs:
-                            client_inputs[self._client_ip_id_combination[data_dict[1]]] = data_object
-                            print "Got client inputs from ", data_dict[1]
-                        else:
-                            print 'client already sent input on this cycle'
+                        if self._client_ip_id_combination[data_dict[1][0]] not in client_inputs:
+                            client_inputs[self._client_ip_id_combination[data_dict[1][0]]] = data_object
+                            print "Got client inputs from ", data_dict[1][0]
+                            print "Client input is ", data_object
+                        #else:
+                            #print 'client already sent input on this cycle'
                 clock.tick(PHYSICS_FPS)
                 self._update_counter += clock.get_time()
 
+            self._update_counter = 0
+
             # TODO: Lasketaan pelin kulku ja muodostetaan paketti clienteille
+            self._game_instance.update()
             #packet_to_client = json_dumps(update_information)
 
             # TODO: Päivitetään tiedot clienteille
             #self._network.server_send_message(packet_to_clients)
 
-    def start_game(self):
+    def start_game(self, game_instance):
         self._waiting_for_client_to_join = False
         self._in_game = True
         self._network.server_send_message(b'Start game')
+        self._game_instance = game_instance
+        self._game_instance.local_player_id = 0
+        print "add players"
+        i = 0
+        for ip, player_id in self._client_ip_id_combination.iteritems():
+            if i % 2:
+                team = "green"
+            else:
+                team = "red"
+            self._game_instance.add_player(player_id, team=team)
+            print player_id
+            i += 1
+        self._game_instance.start()
 
     def shutdown(self):
         self._in_game = False
